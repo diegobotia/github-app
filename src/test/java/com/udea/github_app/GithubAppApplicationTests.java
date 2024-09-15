0b679b9f1e1784ba1a65ff4c64f065aee7b12be4
DB <diego.botia@udea.edu.co>
2024-09-15 11:19:34 -0500

    Primer commit

---
+++ src/test/java/com/udea/github_app/GithubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.github_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
